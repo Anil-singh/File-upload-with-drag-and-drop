0b957fa11f12db661a029977159520abbc89acfc
Anil-singh <Anil-singh@users.noreply.github.com>
2016-07-14 17:23:46 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 	//All constants and folders are defined here
 	define("SITE_URL","localhost");
-	define("WEB_ROOT","/var/www/html/modules/file upload with drag and drop/");
+	define("WEB_ROOT","/var/www/html/modules/file-upload-with-drag-and-drop/");
